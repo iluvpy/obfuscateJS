33e07ce64a6e20b9cac8dcd4d9fa0fc9277ce211
mio manns <miomanns265@gmail.com>
2022-08-17 18:28:13 +0200

    added code to find variable name

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,52 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <stdexcept>
+#include <cstring>
 
 class Obfuscate {
 public:
     Obfuscate(const std::string &filePath) {
         std::ifstream file(filePath);
-        m_content = "";
+        if (!file) {
+            throw std::runtime_error("Error occurred while trying to open file!");
+        }
         std::string line;
         while (getline(file, line)) {
             m_content.append(line);
             m_content.append("\n");
         }
-        std::cout << "file content:\n" << m_content << std::endl;
+        //std::cout << "file content:\n" << m_content << std::endl;
     }
 
-    // TODO: Implement this
     Obfuscate obfuscate() {
+        for (int i = 0; i < m_content.size(); i++) {
+            char currentChar = m_content[i];
+            
+            // obfuscate vars
+            if ((i+2 < m_content.size()) && (currentChar == 'l' || currentChar == 'v')) {
+                std::cout << "current char: " << currentChar << std::endl;
+                std::string word;
+                for (int j = i; j < i+3; j++) {
+                    word.push_back(m_content[j]);
+                }
+                std::cout << "word: " << word << std::endl;
+                if (word == "var" || word == "let") {
+                    std::string varName;
+                    for (int j = i+4; j < m_content.size(); j++) {
+                        char currentJChar = m_content[j]; 
+                        if (j+1 < m_content.size()) {
+                            if (m_content[j+1] == '=') break;
+                        }
+                        varName.push_back(currentJChar);
+                    }
+                    std::cout << "found var: '" << varName << "'" << std::endl;
+
+                    replaceVar(varName, varName.append("yeyeye"), i+4);
+                }
+
+            }
+        }
         return *this;
     }
 
@@ -32,6 +62,14 @@ public:
         return *this;
     }
 
+    // replaces varName with newName
+    // variableStartPointer is the index to the start of the var name inside m_content
+    // let x = 10
+    void replaceVar(const std::string &varName, const std::string &newName, int variableStartPointer) {
+    
+
+    }
+
 private:
     std::string m_content;
     
@@ -39,8 +77,15 @@ private:
 
 int main(int argc, char **argv) {
     if (argc > 1) {
-        std::string filePath = argv[1];
-        Obfuscate obfuscate(filePath);
+        try {
+            std::string filePath = argv[1];
+            Obfuscate obfuscate(filePath);
+            obfuscate.obfuscate();
+        } 
+        catch (const std::exception &e) {
+            std::cerr << e.what() << std::endl;
+            return -1;
+        }
     }
     return 0;
 }
