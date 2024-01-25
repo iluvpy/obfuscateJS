a80b2c3cc0a7e512ac7c58cfc62ac6334f3a5564
mio Manns <miomanns265@gmail.com>
2024-01-25 18:21:30 +0100

    simplified test.js

--- test.js
+++ test.js
@@ -1,27 +1,3 @@
-function coinFlip() {
+function coinFlip() { // hello
     return Math.random() > .5 ? "Heads" : "Tails"; 
-}
-
-function printHello() {
-    console.log("printHello");
-}
-
-function main() {
-    var coinFlips = [];
-    for (var i = 0; i < 100; i++) {
-        coinFlips.push(coinFlip());
-    }
-
-    var heads = 0;
-    var tails = 0;
-    for (var coinflip of coinFlips) {
-        if (coinflip == "Heads") heads++;
-        else tails++;
-    }
-
-    console.log(heads);
-    console.log(tails);
-
-}
-
-main();
+}   
